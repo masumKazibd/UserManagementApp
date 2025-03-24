a287dc8b6d5412dde929a2b5948d9511ecd47321
masumkazibd <masum.kazi.hantmk@gmail.com>
2025-03-24 20:52:41 +0600

    remove unwanted files

---
+++ UserManagementApp/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using UserManagementApp.Models;
+
+namespace UserManagementApp.Data
+{
+    public class AppDbContext : IdentityDbContext<Users>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+    }
+}
---
+++ UserManagementApp/ViewModels/VerifyEmailViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagementApp.ViewModels
+{
+    public class VerifyEmailViewModel
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
